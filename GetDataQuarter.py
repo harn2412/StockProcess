30fc90a5ec3c8b07c82d3b0a8127b19fde335ed2
harn2412 <nguyenchidung2402@gmail.com>
2019-12-04 01:28:26 +0700

    Phien ban 0.2

---
+++ GetDataQuarter.py
@@ -0,0 +1,415 @@
+"""Lay du lieu tu trang web Cafef
+Day la phien ban su dung ten gia tri de lam index thay vi id"""
+
+import requests
+from bs4 import BeautifulSoup as BeauSoup
+import re
+import numpy
+import logging
+import os
+import pandas
+from time import sleep
+from math import ceil
+
+# Cau hinh luu syslog cho chuong trinh
+logger = logging.getLogger('pingdetect')
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s',
+                              datefmt='%y-%m-%d %H:%M:%S')
+
+# Khai bao log Handler de ghi ket qua vao file log
+file = logging.FileHandler('result.log')
+file.setLevel(logging.DEBUG)
+file.setFormatter(formatter)
+logger.addHandler(file)
+
+# Khai bao log Handler de in ket qua ra ngoai man hinh
+console = logging.StreamHandler()
+console.setFormatter(formatter)
+logger.addHandler(console)
+
+
+def convert_to_number(text):
+    """Chuyen doi ket qua thanh so int hoac so float"""
+
+    pattern = re.compile(r'-?[\d,.]+')
+    search_result = pattern.search(text)
+    logger.debug('"search_result" VAR is: %s' % search_result)
+
+    if search_result is not None:
+        num_text = search_result.group().replace(',', '')
+        logger.debug('"num_text" VAR is: %s' % num_text)
+
+        try:  # thu chuyen thanh so int
+            result = int(num_text)
+            logger.info('Chuyen doi qua INT thanh cong, gia tri la: %s' % result)
+        except ValueError:
+            logger.info('Chuyen doi qua INT that bai')
+            try:  # thu chuyen thanh so float
+                result = float(num_text)
+                logger.info('Chuyen doi qua FLOAT thanh cong, gia tri la: %s' % result)
+            except ValueError:
+                logger.warning('Khong the chuyen doi "%s" ra INT hoac FLOAT' % num_text)
+                print('Khong phai Int hoac Float')
+                result = numpy.nan
+
+    else:
+        result = numpy.nan
+
+    return result
+
+
+def get_year_quarter(page_soup):
+    """Lay danh sach cac quy hien co trong trang"""
+    pattern = re.compile(r'Quý (\d)-(\d{4})')
+
+    years_quarter_td = page_soup.find_all('td', {'class': 'h_t'})
+    logger.debug('"years_quarter_td" VAR is: %s' % years_quarter_td)
+
+    if years_quarter_td is not None:
+        years_quarter = [pattern.search(td.text) for td in years_quarter_td]
+
+        year_quarter_slitted = []
+        for year in years_quarter:
+            quarter, year = int(year.group(1)), int(year.group(2))
+            year_quarter_slitted.append((quarter, year))
+
+        logger.info('Lay danh sach cac nam thanh cong, gia tri la: %s' % year_quarter_slitted)
+        return year_quarter_slitted
+
+    else:
+        logger.warning('Khong tim thay danh sach cac nam')
+        return None
+
+
+def get_index_name(text):
+    """Lam dep phan ten cua cac thong so"""
+
+    pattern = re.compile(r'\s*(\w.*\w)\s*')
+    search_result = pattern.search(text)
+
+    logger.debug('"search_result" VAR is: %s' % search_result)
+
+    if search_result is not None:
+
+        index_name = search_result.group(1)
+        logger.info('Chuyen doi thanh cong, ket qua thu duoc: %s' % index_name)
+        return index_name
+
+    else:
+        logger.warning('Khong the lam dep phan ten thong so')
+        return text
+
+
+def get_years(page_soup):
+    """Lay danh sach cac nam hien co trong trang"""
+
+    years_td = page_soup.find_all('td', {'class': 'h_t'})
+    logger.debug('"years_td" VAR is: %s' % years_td)
+
+    if years_td is not None:
+        years = [convert_to_number(td.text) for td in years_td]
+        logger.info('Lay danh sach cac nam thanh cong, gia tri la: %s' % years)
+        return years
+
+    else:
+        logger.warning('Khong tim thay danh sach cac nam')
+        return None
+
+
+def get_data(page_soup):
+    """Lay cac du lieu co trong trang"""
+
+    table_content = page_soup.find('table', {'id': 'tableContent'})
+
+    if table_content is None:
+        logger.warning('Khong tim thay tableContent')
+        return None
+
+    else:
+        logger.debug('"table_content" VAR is: %s' % table_content)
+
+        # Danh sach cac thong so
+        index_names = []
+
+        # Du lieu trong bang
+        datas = []
+
+        rows = table_content.findAll('tr', {'class': ['r_item', 'r_item_a']})
+        logger.debug('"rows" VAR is: %s' % rows)
+
+        for row in rows:
+            logger.debug('"row" VAR is: %s' % row)
+            # bo qua cac hang an vi khong can thiet
+            tr_style = row.get('style')
+            if tr_style is not None and 'display:none' in tr_style:
+                logger.info('Phat hien hang bi an, tien hanh bo qua')
+                continue
+
+            cells = row.find_all('td', {'class': 'b_r_c'})
+            logger.debug('"cells" VAR is: %s' % cells)
+
+            # Cell dau tien ten cua thong so (value name)
+            value_name = get_index_name(cells[0].text)
+            logger.debug('"value_name" VAR is: %s' % value_name)
+            index_names.append(value_name)
+
+            # 4 cell ke tiep la du lieu can lay trong hang
+            row_data = [
+                convert_to_number(cells[1].text),
+                convert_to_number(cells[2].text),
+                convert_to_number(cells[3].text),
+                convert_to_number(cells[4].text),
+            ]
+            logger.debug('"row_data" VAR is: %s' % row_data)
+            datas.append(row_data)
+
+    logger.debug('"index_name" VAR is: %s' % index_names)
+    logger.debug('"data" VAR is: %s' % datas)
+
+    return index_names, datas
+
+
+def create_stock_list(text):
+    """Lam ra danh sach cac ma co phieu"""
+    pattern = re.compile(r'[\w\d]+')
+    stocks = pattern.findall(text)
+    return stocks
+
+
+def create_option_list(text):
+    """Lam ra danh sach cac tuy chon bao cao muon tai ve"""
+    pattern = re.compile(r'[1234]+')
+    options = tuple([int(opt_text) for opt_text in pattern.findall(text)])
+    if 4 in options:
+        return 1, 2, 3
+    else:
+        return options
+
+
+def get_current_year_quarter():
+    """Lay nam va quy hien tai"""
+    from datetime import datetime
+    current_year = datetime.now().year
+    current_quarter = datetime.now().month // 3 + 1
+    return current_year, current_quarter
+
+
+def create_year_quarter_header(last_year, last_quarter, how_many):
+    """Tao mot danh sach cac quy va nam dem lui tuong ung voi nam cuoi cung
+    :type last_year: int
+    :type last_quarter: int
+    :type how_many: int
+    """
+
+    dump_quarters = [4, 3, 2, 1]
+    quarters = dump_quarters[-last_quarter:] + dump_quarters[:-last_quarter]
+
+    # Tao chui cac quy tuong ung do dai can thiet
+    long_quarters = quarters * int(ceil(how_many/4))
+    quarters = long_quarters[:how_many]
+
+    headers = []
+    for quarter in quarters:
+        if quarter == 4:
+            last_year -= 1
+        header = 'Quý {}-{}'.format(quarter, last_year)
+        headers.append(header)
+
+    headers.reverse()  # de cho dung voi bo cuc cua data
+
+    return headers
+
+
+def countdown_quarter(year, quarter, step):
+    """Dem lui quy
+    :type year: int
+    :type quarter: int
+    :type step: int
+    """
+    year_quarter_list = []
+
+    for i in range(step):
+        if quarter > 1:
+            quarter -= 1
+        else:
+            quarter = 4
+            year -= 1
+
+        year_quarter_list.append((year, quarter))
+
+    return year_quarter_list
+
+
+def get_data_of_many_quarter(stock, style, name, how_many_quarter):
+    """Lay du lieu trong nhieu quy va tong hop lai thanh mot bang"""
+
+    df = []
+    year, quarter = get_current_year_quarter()
+
+    url_template = ('http://s.cafef.vn/bao-cao-tai-chinh/{stock_id}/'
+                    '{report_style}/'
+                    '{year}/{quarter}/0/0/0/'
+                    '{report_name}.chn')
+
+    url = url_template.format(
+        stock_id=stock,
+        report_style=style,
+        year=year,
+        quarter=quarter,
+        report_name=name,
+    )
+
+    logger.debug('"url" VAR is: %s' % url)
+
+    columns = create_year_quarter_header(year, quarter, 4)
+    logger.debug('"colums" VAR is: %s' % columns)
+
+    # kiem thu xem Quy hien tai co du lieu hay khong?
+    right_quarter = False
+    retry = 0
+
+    while right_quarter is not True and retry < 3:
+        try_time = 7
+        for i in range(0, try_time):
+            page = requests.get(url)
+            page.raise_for_status()
+            soup = BeauSoup(page.content, 'lxml')
+
+            index_names, data = get_data(soup)
+            dump_df = pandas.DataFrame(data, index=index_names, columns=columns)
+            last_column = dump_df[columns[-1]].tolist()
+
+            if set(last_column) == {numpy.nan}:
+                print('Khong tim thay du lieu, thu tai lai trang ...')
+            else:
+                right_quarter = True
+                df.append(dump_df)
+                break
+            sleep(1)
+
+        else:
+            print('Khong tim thay du lieu trong cot "%s"' % columns[-1])
+            print('Lui lai mot quy de lay du lieu')
+            retry += 1
+            print("Tien hanh cap nhat lai thong tin URL va du lieu tuong ung")
+            year, quarter = countdown_quarter(year, quarter, 1)[-1]
+            url = url_template.format(
+                stock_id=stock,
+                report_style=style,
+                year=year,
+                quarter=quarter,
+                report_name=name,
+            )
+            columns = create_year_quarter_header(year, quarter, 4)
+            print("Da cap nhat lai URL moi voi gia tri: %s" % url)
+            print("Tien hanh thu lai lan %s" % retry)
+    else:
+        print('Khong the tim thay du lieu trong cot cuoi cung, de nghi kiem tra lai')
+
+    if right_quarter is False:
+        return None
+
+    # Neu tim thay quy chinh xac thi tien hanh lay du lieu
+    # Tiep tuc tai cac du lieu con thieu
+    if how_many_quarter > 4:
+        full_quarter_list = countdown_quarter(year, quarter, how_many_quarter)
+        for year, quarter in full_quarter_list[3::4]:  # bat dau tu 3 la vi gia tri dau tien da tien len 1 san
+            url = url_template.format(
+                stock_id=stock,
+                report_style=style,
+                year=year,
+                quarter=quarter,
+                report_name=name,
+            )
+            page = requests.get(url)
+            page.raise_for_status()
+            soup = BeauSoup(page.content, 'lxml')
+
+            index_names, data = get_data(soup)
+            columns = create_year_quarter_header(year, quarter, 4)
+            dump_df = pandas.DataFrame(data, index=index_names, columns=columns)
+
+            df = dump_df.merge(df, how='outer', right_index=True, left_index=True)
+
+    customize_report(df)
+    return df
+
+
+report_style = {
+    1: ('can-doi-ke-toan', 'BSheet', 'bs [{}].csv'),
+    2: ('ket-qua-hoat-dong-kinh-doanh', 'IncSta', 'ist [{}].csv'),
+    3: ('luu-chuyen-tien-te-gian-tiep', 'CashFlow', 'cf [{}].csv'),
+}
+
+
+def customize_report(report):
+    """Hieu chinh lai DataFrame report"""
+
+    # Thay the cac ket qua NaN bang 0
+    report = report.fillna(0)
+    logger.debug('"report" VAR is: % s' % report)
+
+    for column in report.columns:
+        logger.debug('"column" VAR is: %s' % column)
+        # Kiem tra va xoa cac hang khong chua du lieu
+        if set(report[column].to_list()) == {0}:
+            # y nghia la cot chi co chua gia tri 0
+            report = report.drop(column, axis=1)
+            logger.info('Cot "%s" khong co gia tri, tien hanh xoa' % column)
+            continue
+
+        # Doi cac gia tri ve INT cho gon nhe
+        report[column] = pandas.to_numeric(report[column], downcast='signed')
+
+    return report
+
+
+def main():
+    # Duong dan cua thu muc cai dat
+    installed_dir = os.path.dirname(os.path.realpath(__file__))
+    logger.info('Thu muc cai dat la: %s' % installed_dir)
+
+    # Tao thu muc chua ket qua
+    database_dir = os.path.join(installed_dir, 'database')
+    os.makedirs(database_dir, exist_ok=True)
+    logger.info('Khoi tao thu muc chua ket qua: %s' % database_dir)
+
+    # Lay danh sach cac co phieu muon lay du lieu
+    print('Nhap vao cac ma co phieu muon lay du lieu, '
+          'cac ma co phieu phan cach nhau bang dau phay ",". '
+          'Vi du: fpt, aaa, vnm')
+
+    input_text = input('Cac ma co phieu: ')
+    stocks = create_stock_list(input_text)
+
+    print('Nhap vao so thu tu cac loai bao cao ban muon su dung\n'
+          '\t[1] Can doi ke toan\n'
+          '\t[2] Ket qua hoat dong kinh doanh\n'
+          '\t[3] Luu chuyen tien te gian tiep\n'
+          '\t[4] Tai tat ca\n'
+          'Co the tai mot hoac nhieu loai bao cao va phan cach bang dau phay ",". Vi du: 1, 2 hoac 1, 3')
+
+    input_text = input('Tuy chon: ')
+    options = create_option_list(input_text)
+
+    for stock in stocks:
+        # Tao thu muc cho ma co phieu
+        stock_dir = os.path.join(database_dir, stock)
+        os.makedirs(stock_dir, exist_ok=True)
+        logger.info('Khoi tao thu muc cho ma co phieu "%s"' % stock)
+
+        for option in options:
+            report = get_data_of_many_quarter(stock, report_style[option][1], report_style[option][0], 8)
+            logger.debug('"report" VAR is: %s' % report)
+
+            # luu thanh file ket qua
+            for child_report in report:
+                how_long, data_frame = child_report
+                file_patch = os.path.join(stock_dir, report_style[option][2].format(how_long))
+                data_frame.to_csv(file_patch)
+                logger.info('Da luu file "%s" thanh cong' % file_patch)
+
+
+if __name__ == '__main__':
+    main()
